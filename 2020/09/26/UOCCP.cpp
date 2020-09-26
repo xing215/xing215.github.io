e538ec764379a786b1b3710aea4c01435c133cce
xing215 <71538542+xing215@users.noreply.github.com>
2020-09-26 16:14:04 +0700

    Update UOCCP.cpp

--- 2020/09/26/UOCCP.cpp
+++ 2020/09/26/UOCCP.cpp
@@ -2,30 +2,27 @@
 #include <fstream>
 #include <math.h>
 using namespace std;
-bool isPrime(unsigned long n){
-    if (n<2){
+bool isSquare(unsigned long n){
+    if (pow(trunc(sqrt(n)),2) == n){
+        return true;
+    }else{
         return false;
     }
-    for (int i=2; (i*i)<=n; i++){
-        if (n % i == 0){
-            return false;
+}
+void uoccpPrint(unsigned long n){
+    for (unsigned long i=2; i<=(n/2); i++){
+        if ((n%i == 0) && (isSquare(i))){
+            cout<<i<<" ";
         }
     }
-    return true;
-}
-bool isSquare(unsigned long n){
-
+    if (isSquare(n)){
+        cout<<n;
+    }
 }
 int main(){
     freopen("UOCCP.INP","r",stdin);
     freopen("UOCCP.OUT","w",stdout);
     unsigned long n;
     cin>>n;
-    unsigned long temp = maxPrime(n);
-    if (temp == 0){
-        cout<<-1;
-    }else{
-        cout<<temp;
-    }
+    uoccpPrint(n);
 }
-
