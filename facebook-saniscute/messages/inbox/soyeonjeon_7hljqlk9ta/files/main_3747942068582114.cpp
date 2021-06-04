3e5e30cbf5ad541096ebbbb737492fe6b6c15d3a
xing215 <71538542+xing215@users.noreply.github.com>
2021-06-04 17:01:36 +0700

    Upload backup

---
+++ facebook-saniscute/messages/inbox/soyeonjeon_7hljqlk9ta/files/main_3747942068582114.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctime>
+using namespace std;
+void clrscr(){
+    cout<<flush;
+    system("CLS");
+}
+void pause(){
+    cout<<flush;
+    system("PAUSE");
+}
+int century(time_t &a){
+    int temp = a/3153600000;
+    a-= 3153600000*temp;
+    return temp;
+}
+int decade(time_t &a){
+    int temp = a/315569260;
+    a -= 315569260*temp;
+    return temp;
+}
+int year(time_t &a){
+    int temp = a/31556926;
+    a -= 31556926*temp;
+    return temp;
+}
+int month(time_t &a){
+    int temp = a/2629743.83;
+    a -= 2629743.83*temp;
+    return temp;
+}
+int day(time_t &a){
+    int temp = a/86400;
+    a-= 86400*temp;
+    return temp;
+}
+int hour(time_t &a){
+    int temp = a/3600;
+    a -= 3600*temp;
+    return temp;
+}
+int minute(time_t &a){
+    int temp = a/60;
+    a -= 60*temp;
+    return temp;
+}
+int second(time_t &a){
+    return a;
+}
+int main()
+{
+    while (0==0){
+        clrscr();
+        time_t now = time(0);
+        now -= 1595998560;
+        cout<<"Chung ta da tro chuyen cung nhau duoc...\n";
+        cout<<century(now)<<" the ki, "<<decade(now)<<" thap ki, "<<month(now)<<" thang, "<<day(now)<<" ngay, "<<hour(now)<<" gio, "<<minute(now)<<" phut, "<<second(now)<<" giay.\n";
+        cout<<"Nhan phim bat ki de tat";
+        usleep(555555);
+    }
+}
